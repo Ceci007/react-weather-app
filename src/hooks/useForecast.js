3012bec3291512f3fbb5749c1aeb24456664ebc7
Ceci007 <cecibenitezca@gmail.com>
2021-12-02 10:45:10 -0300

    Fetch data from api

--- src/hooks/useForecast.js
+++ src/hooks/useForecast.js
@@ -1,12 +1,41 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import axios from 'axios';
+
+const BASE_URL = 'https://www.metaweather.com/api/location';
+const CROSS_DOMAIN = 'https://the-ultimate-api-challenge.herokuapp.com';
+const REQUEST_URL = `${CROSS_DOMAIN}/${BASE_URL}`;
 
 const useForecast = () => {
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
   const [forecast, setForecast] = useState(null);
 
-  const submitRequest = (location) => {
-    console.log({ location });
+  const getWoeid = async (location) => {
+    const { data } = await axios(`${REQUEST_URL}/search`, { params: { query: location }})
+
+    if(!data || data.length === 0) {
+      setError('There is no such location');
+      return;
+    } 
+
+    return data;
+  }
+
+  const getForecastData = async (woeid) => {
+    const { data } = await axios(`${REQUEST_URL}/${woeid}`);
+    if(!data || data.length === 0) {
+      setError('Something went wrong');
+      return;
+    }
+
+    return data;
+  }
+
+  const submitRequest = async (location) => {
+    const response = await getWoeid(location);
+    const data = await getForecastData(response[0].woeid);
+
+    console.log({ data });
   }
 
   return {
